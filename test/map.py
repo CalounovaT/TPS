626bec7ec0330c363f08ec13ed6a40eb7184c003
CalounovaT <tereza.calounova@gmail.com>
2021-01-20 15:20:45 +0100

    add mapping script

--- test/map.py
+++ test/map.py
@@ -4,15 +4,22 @@ import urllib.parse
 import urllib.request
 import Bio.SeqIO
 import subprocess
+import argparse
+import os.path
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file', help='A required file with UPIs')
+args = parser.parse_args()
 
 # get the UPIs from file
-inp = 'UPIs/upi_00'
+inp = args.file #'UPIs/upi_00'
 with open(inp, 'r') as input_handle:
     upis = [line.strip() for line in input_handle.readlines()]
 
 upi_str = " ".join(upis[:7])
 print(upi_str)
 
+outp = os.path.join('uniprot','seq_' + os.path.basename(inp)[4:])
 
 url = 'https://www.uniprot.org/uploadlists/'
 
@@ -35,12 +42,26 @@ ids = [line.split('\t') for line in mapping.split('\n')]
 ids = [id_entry[1]  for id_entry in ids if len(id_entry) == 2][1:]
 
 print(ids)
-# retrive corresponing organism and sequence
-'uniprot.org/uniprot/?format=tab&columns=id,organism,sequence&query=accession%3A{ID1}+OR+accession%3A{ID2}+OR+accession%3A{ID3}'
-url = 'uniprot.org/uniprot/?format=tab&columns=id,organism,sequence&query=accession%3A'+ids[0]
-for id_entry in ids[1:]:
-    url += '+OR+accession%3A'+id_entry
 
-subprocess.run(['wget', '-O', 'entries2.txt', url])
+if len(ids) == 0:
+    subprocess.run(['touch',outp])
+elif len(ids) == 1:
+    url = 'uniprot.org/uniprot/?format=tab&columns=id,organism,sequence&query=accession%3A'+ids[0]
+    subprocess.run(['wget', '-O', outp, url])
+else:
+    url = 'uniprot.org/uniprot/?format=tab&columns=id,organism,sequence&query=accession%3A'+ids[0]
+    for id_entry in ids[1:]:
+        url += '+OR+accession%3A'+id_entry
+
+    subprocess.run(['wget', '-O', outp, url])
+  
+
+# retrive corresponing organism and sequence
+# 'uniprot.org/uniprot/?format=tab&columns=id,organism,sequence&query=accession%3A{ID1}+OR+accession%3A{ID2}+OR+accession%3A{ID3}'
+#url = 'uniprot.org/uniprot/?format=tab&columns=id,organism,sequence&query=accession%3A'+ids[0]
+#for id_entry in ids[1:]:
+#    url += '+OR+accession%3A'+id_entry
+
+#subprocess.run(['wget', '-O', outp, url])
 
 

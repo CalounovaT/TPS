661a254be1eaa1c24a72ea908905fc4dfd3bfe92
Tereza Čalounová <68317705+CalounovaT@users.noreply.github.com>
2021-03-30 15:12:44 +0200

    change number of jobs

--- run_script.sh
+++ run_script.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 echo "Submitting snakemake jobs to SLURM cluster"
-snakemake --latency-wait 60 --restart-times 1 --jobs 10000 --cluster "sbatch -o {log}.slurm.out -e {log}.slurm.err -n {threads} --mem {params.memory}GB --time=00:01:00 --parsable" --cluster-status ./status.py "$@"
+snakemake --latency-wait 120 --restart-times 1 --jobs 500 --cluster "sbatch -o {log}.slurm.out -e {log}.slurm.err -n {threads} --mem {params.memory}GB --time=01:00:00 --parsable" --cluster-status ./status.py "$@"
